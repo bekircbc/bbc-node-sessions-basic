36cb249ee5b700bc293860a738e42f32777b4a58
Bekir - CDI <b.burakcebecii@gmail.com>
2022-08-16 10:44:54 +0200

    added session and cookies for maintaining the login and expires date added..

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3024;
+
+//setup for sessions
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "tempsecret",
+  })
+);
+
+//setup for cookies
+
+app.use(cookieParser());
 
 const users = [
   {
@@ -22,17 +38,33 @@ app.get("/", (req, res) => {
   res.send("session/cookie basic test");
 });
 
+// app.get("/login/:username", (req, res) => {
+//   const user = users.find((user) => user.username === req.params.username);
+//   if (user) {
+//     res.send(`User identified: ${JSON.stringify(user)}`);
+//   } else {
+//     res.status(500).send("bad login");
+//   }
+// });
+
 app.get("/login/:username", (req, res) => {
   const user = users.find((user) => user.username === req.params.username);
   if (user) {
-    res.send(`User identified: ${JSON.stringify(user)}`);
+    req.session.user = user;
+    req.session.cookie.expires = new Date(Date.now() + 20000);
+    req.session.save();
+    res.send(`User logged in: ${JSON.stringify(user)}`);
   } else {
     res.status(500).send("bad login");
   }
 });
 
 app.get("/current-user", (req, res) => {
-  res.send("no user logged in");
+  if (req.session.user) {
+    res.send(req.session.user);
+  } else {
+    res.send("no user logged in");
+  }
 });
 
 app.listen(PORT, () => {
